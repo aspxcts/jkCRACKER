6724b5c3892713b6e8a13a4aeb5aeebd7ebbe652
azamsafir <azamsafir@gmail.com>
2023-09-13 18:54:33 -0500

    (MASSIVE UPDATE): updated to not use google api key

--- googlesearcher.py
+++ googlesearcher.py
@@ -3,11 +3,20 @@ import pyautogui
 import random
 import sys
 import time
+import re
+from collections import defaultdict
 from colorama import Fore
+from scanner import scan
 
 y = Fore.LIGHTYELLOW_EX
 b = Fore.LIGHTCYAN_EX
 w = Fore.LIGHTWHITE_EX
+
+
+def find_words(word_index, letters):
+    idx = tuple(sorted(letters))
+    return word_index[idx]
+
 
 def loader():
     l = ['|', '/', '-', '\\']
@@ -15,48 +24,30 @@ def loader():
         sys.stdout.write(f"""\r{y}[{b}#{y}]{w} Cooking... {i}""")
         sys.stdout.flush()
         time.sleep(0.2)
-def createurl():
-    from main import key
-    from main import engineid
-    url = 'https://www.googleapis.com/customsearch/v1?key='
-    url += key
-    url += '&cx='
-    url += engineid
-    url += '&q=words%20that%20have%20'
-    return url
+
 
 def searchandretreive(addon):
-    #loader()
-    count = 0
-    while True:
-        url = createurl()
-        url += addon
-        useablewords = []
-        loweredaddon = addon.lower()
-        response = requests.get(url)
-        response_json = response.json()
-        words = response_json['items'][0]['snippet']
-        words = words.split()
-        for word in words:
-            if word == loweredaddon:
-                index = words.index(word)
-                words.pop(index)
-            if loweredaddon in word:
-                useablewords.append(word)
-        for word in useablewords:
-            if word == loweredaddon:
-                index = useablewords.index(word)
-                useablewords.pop(index)
+    loweredaddon = addon.lower()
+    response = requests.get("https://raw.githubusercontent.com/first20hours/google-10000-english/master/20k.txt")
+    words = response.text.splitlines()
+    word_list = list(words)
+    word_dict = {word: word for word in word_list}
+    found_words = [word for word in word_dict if re.search(loweredaddon, word)]
+    word = random.choice(found_words)
 
-        word = random.choice(useablewords)
-        wordindex = useablewords.index(word)
-        interval = random.uniform(0.005,0.01)
+    if loweredaddon in word:
+        wordindex = found_words.index(word)
+        interval = random.uniform(0.005, 0.01)
         pyautogui.moveTo(810, 1042)
         pyautogui.leftClick()
         for char in word:
             pyautogui.typewrite(char, interval=interval)
-            interval = random.uniform(0.005,0.01)
+            interval = random.uniform(0.005, 0.01)
         pyautogui.press('enter')
-        useablewords.pop(wordindex)
-        count += 1
+        found_words.pop(wordindex)
+    else:
+        wordindex = found_words.index(word)
+        found_words.pop(wordindex)
+        searchandretreive(addon)
+
 
